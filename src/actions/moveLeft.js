929b7eee5f21d2290ef4035a8634bf14b1f8c7d8
James Moore <jameslmoore@gmail.com>
2017-09-07 23:22:30 -1000

    move left

---
+++ src/actions/moveLeft.js
@@ -0,0 +1,25 @@
+/* jshint esversion: 6 */
+
+import React from 'react';
+
+export const MOVE_LEFT = "MOVE_LEFT"
+
+export default (originalCard) =>{
+  const card = { ...originalCard };
+  switch (card.status){
+    case "progress":
+      card.status = "queue"
+      break;
+    case "done":
+      card.status = "progress"
+      break;
+    default:
+
+
+  }
+  console.log('card status is', card);
+  return {
+      type: MOVE_LEFT,
+      card: card
+    };
+};
