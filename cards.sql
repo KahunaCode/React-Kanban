901ade05989b460d4458e5c5fb8670007f9925bb
James Moore <jameslmoore@gmail.com>
2017-09-09 19:44:30 -1000

    dummy start data

---
+++ cards.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Cards" VALUES
+(default, 'DB make Styles', 'medium', 'queue', 'james', 'notJames', NOW(), NOW()),
+(default, 'DB second Styles', 'medium', 'progress', 'DBjames', 'AlsoJames', NOW(), NOW()),
+(default, 'DB Other Styles', 'medium', 'done', 'etc james', 'notJames', NOW(), NOW());
